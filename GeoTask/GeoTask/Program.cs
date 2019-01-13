d2227620981455619b1b29d6a141b1eb5ff8f8f2
Kazantsev85 <kazantsev_85@mail.ru>
2019-01-13 23:06:03 +0300

    Создано основное меню. Добавлено два пустых временных метода для двух задач и три кейса для вызова методов.

---
+++ GeoTask/GeoTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение прямых и обратных геодезических задач");
+            Console.ReadKey();
+            string m;
+            do
+            {
+                Menu(); // Вызывает меню
+                m = Console.ReadLine();
+                switch (m)
+                {
+                    case "1":
+                        Task1();
+                        break;
+                    case "2":
+                        Task2();
+                        break;                    
+                    case "0":
+                        Console.WriteLine("До свидания!");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Default();
+                        break;
+                }
+            } while (m != "0");
+        }
+        static void Menu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Выберите задачу:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Нажите 1 для решения прямой геодезической задачи и 2 для решения обратной геодезической задачи");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Выход: 0");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Task1()
+        {
+            Console.WriteLine("Решение прямой геодезической задачи в разработке");
+            Console.ReadKey();
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Решение обратной геодезической задачи в разработке");
+            Console.ReadKey();
+        }
+        static void Default()
+        {
+            Console.WriteLine("Неверно!");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
