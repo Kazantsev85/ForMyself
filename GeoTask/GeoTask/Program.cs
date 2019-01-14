ebefabb053528528eae19cdc83c0ae4817b5f820
Kazantsev85 <kazantsev_85@mail.ru>
2019-01-14 03:32:03 +0300

    Прямая задача решается правильно. Добавлено форматирование в окно вывода результатов для отображения координат до сотых.

--- GeoTask/GeoTask/Program.cs
+++ GeoTask/GeoTask/Program.cs
@@ -99,11 +99,12 @@ namespace GeoTask
             Console.WriteLine("Enter Distance");
             double L = InputDouble();
 
-            Console.WriteLine($"Данные введены: Easting {Y}, Nothing {X}, Direction {Da}, Distance {L}");
+            //Console.WriteLine($"Данные введены: Easting {Y}, Nothing {X}, Direction {Da}, Distance {L}");
 
             Point A = new Point(X, Y);            
             Point B = GEOTask.FirstTask(A, Da, L);
-            Console.WriteLine($"{B.x},{B.y}");
+            Console.WriteLine("Координаты второй точки:");
+            Console.WriteLine($"Nothing: {B.x:f2}; Easting: {B.y:f2}");
             
             Console.ReadKey();
         }
