e455eeb42ae86dcae80e826c08ab1f05453901e5
Kazantsev85 <kazantsev_85@mail.ru>
2019-01-17 07:05:51 +0300

    новый класс

---
+++ GeoTask/GeoTask/PointGraph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTask
+{
+    static class PointGraph
+    {
+        public static double X1 { get; set; }
+        public static double X2 { get; set; }
+        public static double Y1 { get; set; }
+        public static double Y2 { get; set; }
+    }
+}
